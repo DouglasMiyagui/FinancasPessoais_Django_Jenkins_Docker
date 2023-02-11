9a7dcb215246b5001ca41ac3b2c245fa646fa6e6
DouglasMiyagui <d2.mk@hotmail.com>
2023-02-11 23:43:51 +0000

    staticfiles

--- setup/settings.py
+++ setup/settings.py
@@ -128,12 +128,10 @@ DATE_FORMAT = 'Y-m-d'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "setup/static")
+    os.path.join(BASE_DIR, "static")
 ]
 
 # Default primary key field type
