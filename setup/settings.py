f595c084228ebadc8dd0f1993bffbdf480d77ee3
DouglasMiyagui <d2.mk@hotmail.com>
2023-03-25 00:54:09 +0000

    CRUD - Refatorado

--- setup/settings.py
+++ setup/settings.py
@@ -133,7 +133,7 @@ DATE_FORMAT = 'Y-m-d'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
