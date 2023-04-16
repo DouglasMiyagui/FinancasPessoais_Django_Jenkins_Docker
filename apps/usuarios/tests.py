cc35e26053577d70ba388c152d4c51c40e131a2b
DouglasMiyagui <d2.mk@hotmail.com>
2023-04-16 19:54:13 +0000

    Testes test_models

--- apps/usuarios/tests.py
+++ apps/usuarios/tests.py
@@ -1,1 +1,19 @@
+from django.test import TestCase, RequestFactory
+from pessoais.models import Receita, Despesa
+from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 
+class UsuariosTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_login(self):
+            """Testa o cadastro do usuário e o login do mesmo"""
+            self.user = User.objects.create_user(
+                username='test',email='teste@teste.com', password='test')
+            self.user.save()
+
+            # Logging
+            login = self.client.login(username='test', password='test')
+            self.assertEquals(login, True)
